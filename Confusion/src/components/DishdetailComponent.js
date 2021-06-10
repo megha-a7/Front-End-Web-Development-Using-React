c2f09ed22412a6e23006f40076a710d066c29072
megha-a7 <82528894+megha-a7@users.noreply.github.com>
2021-06-10 22:58:32 +0530

    Secondass1

--- Confusion/src/components/DishdetailComponent.js
+++ Confusion/src/components/DishdetailComponent.js
@@ -1,29 +1,15 @@
-import React, { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import React from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem, CardBody, CardText } from "reactstrap";
+import { Link } from 'react-router-dom';
 
-class DishDetail extends Component{
-    constructor(props) {
-        super(props);
-
-        console.log(props);
-        
-        // stores iproperties of this component
-        this.state = {
-            selectedDishDetail: this.props.dsdetail
-        };
-
-
-    }
-
-    renderDish(dish) {
-
+    function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardBody>
-                            <CardTitle> {dish.name} </CardTitle>
+                            <CardTitle> {dish.name}</CardTitle>
                             <CardText> {dish.description} </CardText>
                         </CardBody>
                     </Card>
@@ -37,7 +23,7 @@ class DishDetail extends Component{
         }
     }
 
-    renderComments(comments){
+    function RenderComments({comments}){
         if (comments == null) {
             return (<div></div>)
         }
@@ -68,26 +54,41 @@ class DishDetail extends Component{
     }
 
 
-    render(){
-        const dish = this.props.dish
+    const DishDetail = (props) => {
 
-        console.log(dish);
+        const dish = props.dish
         
+    
         if (dish == null) {
             return (<div></div>);
         }
 
-        const dishItem = this.renderDish(dish);
-        const dishComment = this.renderComments(dish.comments);
-
         return (
-            <div className='row'>
-                {dishItem}
-                {dishComment}
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to="/menu">Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            { props.dish.name }
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+
+                    <div className="col-12">
+                        <h3> {props.dish.menu}</h3>
+                        <hr />
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <RenderDish dish={ props.dish } />
+                    <RenderComments comments={ props.comments } />
+                </div>
             </div>
         )
     }
 
-}
+
 
 export default DishDetail;
